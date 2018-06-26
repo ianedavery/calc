263a95925dfb3df7cb033c002dff352be464b90c
Ian Avery <ianedavery@gmail.com>
2018-06-26 11:41:41 -0700

    created a subtraction button

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 $("document").ready(function(){
 
-	let numArray = [],
-		calcArray = [];
+	let calcString = "";
 
 	$("#add_button").click(function(){
-		let num = $("#number_input").val(),
-			sum;
-		if(!calcArray.length){
-			calcArray.push(num);
-		}
-		else{
-			calcArray.push("+");
-			calcArray.push(num);
-		}
-		if(numArray.length){
-			let lastNum = numArray.pop();
-			sum = parseInt(num) + parseInt(lastNum);
-			numArray.push(sum);
-		}
-		else{
-			numArray.push(parseInt(num));
-		}
-		$("#number_output").html(`<span>${calcArray.join(' ')}</span>`);
+		$("#number_input").val() === "" ? calcString += "0 + " : calcString += `${$("#number_input").val()} + `;
+		$("#number_output").html(`<span>${calcString.substring(0, calcString.length - 2)}</span>`);
+		$("#number_input").val("");
+	});
+
+	$("#subtract_button").click(function(){
+		$("#number_input").val() === "" ? calcString += "0 - " : calcString += `${$("#number_input").val()} - `;
+		$("#number_output").html(`<span>${calcString.substring(0, calcString.length - 2)}</span>`);
 		$("#number_input").val("");
 	});
 
 	$("#equals_button").click(function(){
-		$("#number_output").val("");
-		$("#number_output").append(numArray[numArray.length - 1]);
-		numArray = [];
-		calcArray = [];
+		$("#number_input").val() === "" ? calcString += "0" : calcString += $("#number_input").val();
+		$("#number_output").html(`<span>${eval(calcString)}</span>`);
+		$("#number_input").val("");
+		calcString = "";
 	});
 
 });
